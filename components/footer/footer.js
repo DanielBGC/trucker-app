4aa3740314d49ab865d04b024849832c9fa9df26
DanielBGC <daniel.borba-@hotmail.com>
2021-06-20 20:52:47 -0300

    Caminho relativo para resolver bug do github pages

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -10,22 +10,26 @@ function changeCurrentPage(action) {
   let currentIcon;
 
   if(action == "home") {
-    window.location.href = "/components/menu/menu-inicial.html"
+    // window.location.href = "/components/menu/menu-inicial.html"
+    window.location.href = "../menu/menu-inicial.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
   if(action == "perfil") {
-    window.location.href = "/components/perfil/perfil.html"
+    // window.location.href = "/components/perfil/perfil.html"
+    window.location.href = "../perfil/perfil.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
   if(action == "estabelecimentos") {
-    window.location.href = "/components/estabelecimentos/estabelecimentos.html"
+    // window.location.href = "/components/estabelecimentos/estabelecimentos.html"
+    window.location.href = "../estabelecimentos/estabelecimentos.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
   if(action == "forum") {
-    window.location.href = "/components/forum/forum.html"
+    // window.location.href = "/components/forum/forum.html"
+    window.location.href = "../forum/forum.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
