8dd913b67ec177b70bdcdf6e1d19acd5b22218b1
DanielBGC <daniel.borba-@hotmail.com>
2021-06-21 00:30:16 -0300

    Ajuster no botão de no header

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,30 +5,28 @@ iconsFooter.forEach(icon => {
     })
 })
 
+const currentHostName = window.location.hostname
+
 function changeCurrentPage(action) {
   console.log(action)
   let currentIcon;
 
   if(action == "home") {
-    // window.location.href = "/components/menu/menu-inicial.html"
     window.location.href = "../menu/menu-inicial.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
   if(action == "perfil") {
-    // window.location.href = "/components/perfil/perfil.html"
     window.location.href = "../perfil/perfil.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
   if(action == "estabelecimentos") {
-    // window.location.href = "/components/estabelecimentos/estabelecimentos.html"
     window.location.href = "../estabelecimentos/estabelecimentos.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
 
   if(action == "forum") {
-    // window.location.href = "/components/forum/forum.html"
     window.location.href = "../forum/forum.html"
     currentIcon = document.querySelector("footer .icon." + action)
   }
